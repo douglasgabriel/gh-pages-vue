af0fc4b6c221012dfe17cb7e7706e3603a4dfabe
Douglas Gabriel <douglasxgabriel@gmail.com>
2020-01-26 16:36:02 -0300

    Stylizing main content of AboutPage

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,7 @@ html, body, #app {
   width: 100%;
   height: 100%;
   margin: 0;
+  background-color: #F2F7FA;
 }
 
 #app {
--- src/pages/about/AboutPage.vue
+++ src/pages/about/AboutPage.vue
@@ -4,7 +4,7 @@
       <div class="content row">
         <section class="left-actions column">
           <a href="/#/">
-            <i class="icon ion-ios-arrow-back"></i>
+            <i class="icon ion-md-arrow-back"></i>
           </a>
         </section>
         <section class="content column">
@@ -19,7 +19,27 @@
       </div>
     </header>
 
-    <main></main>
+    <main class="z-depth-5">
+      <div class="content">
+        <section class="social-links">
+          <ul>
+            <li>
+              <i class="icon ion-logo-github"></i>
+            </li>
+            <li>
+              <i class="icon ion-logo-linkedin"></i>
+            </li>
+          </ul>
+        </section>
+        <section class="cover-letter">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores, deleniti fugiat quis quas voluptatibus beatae? Alias perferendis suscipit vero odio. Vero accusantium aliquid earum voluptates iste? Eum doloremque vero temporibus!
+            Sint ipsa dolore excepturi optio molestiae deserunt omnis ut exercitationem cupiditate autem expedita error amet rem ad at obcaecati, ullam id sed perspiciatis illo unde? Sint blanditiis nesciunt delectus minus?
+            Itaque perferendis soluta, quae asperiores, excepturi eius ipsam quisquam placeat reprehenderit beatae consequuntur nulla dicta optio odio reiciendis natus veritatis delectus officia vero culpa quaerat ratione corporis commodi assumenda? Possimus.
+          </p>
+        </section>
+      </div>
+    </main>
 
     <footer></footer>
   </div>
@@ -39,7 +59,7 @@ export default {
 
 <style scoped lang="scss">
 header {
-  background: linear-gradient(#1f639a, #183f5f);
+  background: linear-gradient(#3859e1, #4f71f4);
   color: white;
 
   a {
@@ -50,18 +70,40 @@ header {
     padding: 20px;
 
     h1 {
-      color: #3caea2;
+      color: #6aead0;
+      font-size: 100px;
+      display: inline;
     }
 
     .me {
-      width: 150px;
-      height: 150px;
+      width: 201px;
 
       background-position: center;
       background-position-x: -65px;
       background-size: cover;
 
       border-radius: 50%;
+    }
+  }
+}
+
+main {
+  .content {
+    color: #a1aab8;
+
+    .social-links {
+      color: #6aead0;
+
+      ul {
+        padding: 0;
+
+        li {
+          font-size: 30px;
+          list-style: none;
+          display: inline-block;
+          margin-right: 10px;
+        }
+      }
     }
   }
 }
